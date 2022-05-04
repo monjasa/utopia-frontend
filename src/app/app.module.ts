82161d028b551bb34046d41239cc54ea51309d6c
arthur.kashapov <arthur.kashapov@kindgeek.com>
2022-05-04 18:00:15 +0300

    Install Bootstrap and create a style hierarchy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,14 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
