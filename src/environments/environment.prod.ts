e5917b4fbbbdf33fc199d213874f6509d43fd2f9
monjasa <monja.nya@gmail.com>
2022-06-03 09:17:24 +0300

    Generalize application environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://api-utopia-theatre.herokuapp.com/api',
   firebase: {
     projectId: 'utopia-theatre',
     appId: '1:348711351609:web:4644abde6fb1ad81a164e8',
